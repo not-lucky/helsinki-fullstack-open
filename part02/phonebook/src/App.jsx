3060a4ed68492e234c4793b61a8c5a039472c2f4
not-lucky <ayushrwt801@gmail.com>
2026-01-10 15:36:19 +0530

    Exercise 2.12-2.15

--- part02/phonebook/src/App.jsx
+++ part02/phonebook/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
-import Filter from './Filter';
-import PersonForm from './PersonForm';
-import Persons from './Persons';
-import axios from 'axios';
+import Filter from './components/Filter';
+import PersonForm from './components/PersonForm';
+import Persons from './components/Persons';
+import personsService from './services/persons';
+
 
 const App = () => {
   const [ persons, setPersons ] = useState( [] );
 
-  const [ newPerson, setNewPerson ] = useState( { name: '', number: '', id: 100 } );
+  const [ newPerson, setNewPerson ] = useState( { name: '', number: '' } );
   const [ filter, setFilter ] = useState( '' );
 
 
@@ -15,24 +16,65 @@ const App = () => {
     event.preventDefault();
     // console.log( 'newName', newName );
 
-    if ( persons.some( person => person.name === newPerson.name ) ) {
-      alert( `${ newPerson.name } is already added to phonebook` );
-      return;
-    }
+    const existingPerson = persons.find(
+      person => person.name === newPerson.name
+    );
 
-    setPersons( persons.concat( newPerson ) );
-    setNewPerson( { name: '', number: '', id: newPerson.id + 1 } );
+    if ( existingPerson ) {
+      if ( confirm( `${ newPerson.name } is already added to phonebook, replace the old numbere with the new one?` ) ) {
+
+        const updatedPerson = { ...existingPerson, number: newPerson.number };
+
+        personsService
+          .update( existingPerson.id, updatedPerson )
+          .then( returnedPerson => {
+            setPersons( persons.map( person =>
+              person.id !== existingPerson.id ? person : returnedPerson
+            ) );
+            setNewPerson( { name: '', number: '' } );
+          } );
+      }
+      return;
+    };
+
+    personsService
+      .create( newPerson )
+      .then( returnedPerson => {
+        setPersons( persons.concat( returnedPerson ) );
+        setNewPerson( { name: '', number: '' } );
+      } );
+
   };
 
+
+  const handleDelete = ( id ) => {
+    console.log( 'id', id );
+    if ( !window.confirm( `Sure wanna delete this person with id ${ id }` ) ) {
+      return;
+    }
+    console.log( 'uwu' );
+
+
+    personsService
+      .purge( id )
+      .then( () => {
+        setPersons( persons.filter( person => person.id !== id ) );
+      } )
+      .catch( () => {
+        alert( 'person was already purged from the server.' );
+      } );
+  };
 
   const filteredPersons = filter === '' ? persons : persons.filter( ( person ) => person.name.toLocaleLowerCase().includes( filter.toLocaleLowerCase() ) );
 
 
+
+
   useEffect( () => {
-    axios
-      .get( 'http://localhost:3001/persons' )
-      .then( ( response ) => {
-        setPersons( response.data );
+    personsService
+      .getAll()
+      .then( newData => {
+        setPersons( newData );
       } );
 
   }, [] );
@@ -49,7 +91,7 @@ const App = () => {
 
       <h2>Numbers</h2>
 
-      <Persons filteredPersons={ filteredPersons } />
+      <Persons filteredPersons={ filteredPersons } handleDelete={ handleDelete } />
 
     </div>
   );
--- part02/phonebook/src/Persons.jsx
+++
@@ -1,9 +0,0 @@
-const Persons = ( { filteredPersons } ) => {
-  return (
-    <>
-      { filteredPersons.map( person => <li key={ person.id }> { person.name } { person.number } </li> ) }
-    </>
-  );
-};
-
-export default Persons;
